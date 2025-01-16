c05356afbdb46bbe3cfc102867b99d87d923a1b2
me <slavaveto@gmail.com>
2025-01-15 22:42:01 -0300

    перенес в myapps

--- app/components/PageListBox.tsx
+++ app/components/PageListBox.tsx
@@ -1,4 +1,6 @@
-import { Listbox, ListboxItem } from '@nextui-org/react';
+import { MoreHorizontal, MoreVertical } from 'lucide-react'; // Иконка с тремя точками
+import { Listbox, ListboxItem, Dropdown, DropdownItem, DropdownTrigger, DropdownMenu, Button } from "@nextui-org/react";
+import { Link } from '@nextui-org/link';
 
 interface Page {
     page_key: string;
@@ -10,9 +12,11 @@ interface PageListboxProps {
     pages: Page[];
     selectedPage: string | null;
     onSelectPage: (pageKey: string) => void;
+    onRename: (pageKey: string) => void; // Функция для переименования
+    onCopy: (pageKey: string) => void; // Функция для копирования
 }
 
-export default function PageListBox({ pages, selectedPage, onSelectPage }: PageListboxProps) {
+export default function PageListBox({ pages, selectedPage, onSelectPage, onCopy, onRename }: PageListboxProps) {
     return (
         <Listbox
             variant="faded"
@@ -28,9 +32,29 @@ export default function PageListBox({ pages, selectedPage, onSelectPage }: PageL
                         ${selectedPage === page.page_key ? 'bg-default-100' : ''} 
                     `}
                 >
+                    <div className={"flex flex-row justify-between items-center"}>
                     <span className={page.btn_type === 'image' ? 'font-semibold' : ''}>
                         {page.page_key}
                     </span>
+                    {/* Иконка с тремя точками */}
+                    {/*<MoreHorizontal size={16} className="text-default-300 hover:text-primary-500 cursor-pointer" />*/}
+                        {/* Выпадающий список */}
+                        <Dropdown>
+                            <DropdownTrigger className="px-0">
+                                <Link className="text-default-400 hover:text-primary-700 transition">
+                                    <MoreVertical size={16} /></Link>
+
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="Действия">
+                                <DropdownItem key="rename" onClick={() => onRename(page.page_key)}>
+                                    Переименовать
+                                </DropdownItem>
+                                <DropdownItem key="copy" onClick={() => onCopy(page.page_key)}>
+                                    Скопировать
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                </div>
                 </ListboxItem>
             ))}
         </Listbox>
